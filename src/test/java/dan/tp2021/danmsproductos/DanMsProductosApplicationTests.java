7e15a8618c74bd8d138fa183889be36b5ec5373c
Riquelme Ciro <ciroriquelme05@gmail.com>
2021-05-12 01:17:26 -0300

    first commit

---
+++ src/test/java/dan/tp2021/danmsproductos/DanMsProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.tp2021.danmsproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
